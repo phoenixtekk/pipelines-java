c4d453e0c1103318a32b58f9437033951bcd6e6f
Phoenixtekk <lacy@phoenixtekk.io>
2022-11-14 13:00:23 -0700

    first version commit v1

---
+++ PowerShell.ps1
@@ -0,0 +1,19 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.211
+	 Created on:   	11/14/2022 12:53 PM
+	 Created by:   	Lacy
+	 Organization: 	Phoenixtekk
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+param ([string]$message = "Hello World"
+	
+)
+
+Write-Output $message
+
