dd4d0c64bcd4d9507d92187712fc47848ad70e4e
Phoenixtekk <lacy@phoenixtekk.io>
2022-11-15 08:20:50 -0700

    Adding powershell file script

---
+++ PowerShellScript.ps1
@@ -0,0 +1,18 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.211
+	 Created on:   	11/15/2022 8:07 AM
+	 Created by:   	Lacy
+	 Organization: 	Phoenixtekk
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+param ([string]$message = "Hello World"
+	
+)
+
+Write-Output $message
